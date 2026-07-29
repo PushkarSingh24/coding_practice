a0afcbccde0c7ed01dbfbde3fa69a98c4921e776
Pushkar Singh <pushkarg2000@gmail.com>
2026-07-30 01:09:34 +0530

    Add Divisibility Problem [A] from Codeforces

---
+++ 1328/A - Divisibility Problem/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+ 
+            if (a % b == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(b - (a % b));
+            }
+        }
+ 
+        sc.close();
+    }
+}
