4036dfc99ffff4db509b953735d6068a4984a780
Pushkar Singh <pushkarg2000@gmail.com>
2026-08-06 19:20:59 +0530

    Add Magnets [A] from Codeforces

---
+++ 344/A - Magnets/solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int groups = 1; 
+        String prev = sc.next();
+        for (int i = 1; i < n; i++){
+            String curr = sc.next();
+            if (!curr.equals(prev)){
+                groups++;
+            }
+            prev = curr; 
+        }
+        System.out.println(groups); 
+        sc.close();
+    }
+}
